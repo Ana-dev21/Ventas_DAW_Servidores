a26026a9a9233fc978936b09be8d17d81dfd3701
anni <adarvoy@gmail.com>
2023-03-12 12:38:05 +0100

    Create project

---
+++ proyecto_examen_final/src/main/java/com/edix/proyecto_examen_final/ProyectoExamenFinalApplication.java
@@ -0,0 +1,13 @@
+package com.edix.proyecto_examen_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoExamenFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoExamenFinalApplication.class, args);
+	}
+
+}
---
+++ proyecto_examen_final/src/test/java/com/edix/proyecto_examen_final/ProyectoExamenFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edix.proyecto_examen_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoExamenFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
